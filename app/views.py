aa446005510f9991cc1c515884eb9eeaa53b0609
samiulalimsaad <samiulalimsaad@gmail.com>
2024-03-24 17:24:22 +0600

    home page updated for dynamic doctor

--- app/views.py
+++ app/views.py
@@ -14,6 +14,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["featured_doctors"] = Doctor.objects.all()[:3]
+        context["doctors"] = Doctor.objects.all()
         context["reviews"] = Review.objects.all()[:2]
         return context
 
