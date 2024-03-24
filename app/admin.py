aa37cbe20e8e85587cf37330451fa491a775ffdb
samiulalimsaad <samiulalimsaad@gmail.com>
2024-03-24 21:14:10 +0600

    custom admin interface added

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,34 @@
+import os
+
+import requests
 from django.contrib import admin
 
 from app.models import Appointment, Doctor, Patient, Review
 
-admin.site.register(Doctor)
+from .models import Doctor
+
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ["user", "expertise", "profile_picture"]
+    search_fields = ["user__username", "expertise"]
+
+    def save_model(self, request, obj, form, change):
+        if form.cleaned_data["profile_picture"]:
+            imgbb_api_key = os.getenv("IMGBB")
+            profile_picture = form.cleaned_data["profile_picture"]
+            response = requests.post(
+                "https://api.imgbb.com/1/upload",
+                params={"key": imgbb_api_key},
+                files={"image": profile_picture.file},
+            )
+            print(response.status_code)
+            if response.status_code == 200:
+                url = response.json()["data"]["url"]
+                obj.profile_picture = url
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(Patient)
 admin.site.register(Appointment)
 admin.site.register(Review)
