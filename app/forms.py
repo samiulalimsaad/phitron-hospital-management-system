4ff356d089279b508dfe74c40e8f8118d9dc6371
samiulalimsaad <samiulalimsaad@gmail.com>
2024-03-24 21:29:23 +0600

    DoctorForm added

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from .models import Appointment, Review
+from .models import Appointment, Doctor, Review
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -23,3 +23,9 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ["rating", "comment"]
+
+
+class DoctorForm(forms.ModelForm):
+    class Meta:
+        model = Doctor
+        fields = ["expertise", "profile_picture"]
